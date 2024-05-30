ed403d53f787ec10b5244700709946334fcd3804
Young Yu <young@youngs-mbp-3.attlocal.net>
2024-05-30 00:40:02 -0700

    added http bypass list, with the repo of sysproxy-cmd-darwin

--- sysproxy_darwin.go
+++ sysproxy_darwin.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"fmt"
 	"os/exec"
+	"strings"
 	"syscall"
 
 	"github.com/getlantern/byteexec"
@@ -11,6 +12,7 @@ import (
 )
 
 // Note this is a universal binary that runs on amd64 and arm64
+//
 //go:embed binaries/darwin/sysproxy
 var sysproxy []byte
 
@@ -32,4 +34,52 @@ func detach(cmd *exec.Cmd) {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Setpgid: true,
 	}
+}
+
+func SetBypass(service, list string) error {
+	mu.Lock()
+	defer mu.Unlock()
+	if be == nil {
+		return fmt.Errorf("call EnsureHelperToolPresent() first")
+	}
+
+	cmd := be.Command("setbypass", service, list)
+	err := run(cmd)
+	if err != nil {
+		log.Errorf("Unable to set bypasslist %v", err)
+		return err
+	}
+	return nil
+}
+
+func UnSetBypass(service string) error {
+	mu.Lock()
+	defer mu.Unlock()
+	if be == nil {
+		return fmt.Errorf("call EnsureHelperToolPresent() first")
+	}
+
+	cmd := be.Command("unsetbypass", service)
+	err := run(cmd)
+	if err != nil {
+		log.Errorf("Unable to set bypasslist %v", err)
+		return err
+	}
+	return nil
+}
+
+func GetBypass(service string) (string, error) {
+	mu.Lock()
+	defer mu.Unlock()
+	if be == nil {
+		return "", fmt.Errorf("call EnsureHelperToolPresent() first")
+	}
+
+	cmd := be.Command("getbypass", service)
+	out, err := cmd.Output()
+	if err != nil {
+		log.Errorf("Unable to get bypasslist %v", err)
+		return "", err
+	}
+	return strings.TrimRight(string(out), "\r\n"), nil
 }
